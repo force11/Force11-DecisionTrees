5a69a2aca993caed38254ef0618eabdc59616275
Michael Bar-Sinai <mich.barsinai@gmail.com>
2017-05-15 15:33:18 +0300

    Added is-data-FAIR

---
+++ is-data-FAIR/definitions_FAIR_MM.ts
@@ -0,0 +1,15 @@
+DataTags: consists of DataLevel, Assertions.
+
+DataLevel: one of L1[Fully principles compliant:  Open in the sense of the Open definition:  Free to use and reuse.],
+                  L2[As compliant as possible given regulatory or safety concerns:  Only minimal required barriers for access, e.g, access to human subject data for qualified people.],
+                  L3[Somewhat principles compliant:  Digital footprint, but significant barriers for access and reuse, e.g., landing page with descriptive metadata but no access or access that requires significant effort or expense.],
+                  L4[Not principles compliant:  No digital footprint;  e.g., dark data].
+
+Assertions: consists of InRepository, IsSensitive.
+
+Media: one of analog, digital.
+
+IsSensitive: one of yes [Dataset contains data that can cause harm to the data subjects if distributed freely.],
+                    no [Dataset will not cause harm to the data subjects, even when distributed freely.].
+
+InRepository: TODO.
