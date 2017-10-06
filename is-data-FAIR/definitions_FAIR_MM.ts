153a38c07957108be0bf744ab93447bb3a33dca0
memartone <mmartone@ucsd.edu>
2017-10-05 23:16:41 -0700

    Updated Compliance levels for FAIR

--- is-data-FAIR/definitions_FAIR_MM.ts
+++ is-data-FAIR/definitions_FAIR_MM.ts
@@ -1,9 +1,9 @@
 DataTags: consists of DataLevel, Assertions.
 
-DataLevel: one of L1[Fully principles compliant:  Open in the sense of the Open definition:  Free to use and reuse.],
-                  L2[As compliant as possible given regulatory or safety concerns:  Only minimal required barriers for access, e.g, access to human subject data for qualified people.],
-                  L3[Somewhat principles compliant:  Digital footprint, but significant barriers for access and reuse, e.g., landing page with descriptive metadata but no access or access that requires significant effort or expense.],
-                  L4[Not principles compliant:  No digital footprint;  e.g., dark data].
+DataLevel: one of L1[Fully principles compliant:  Findable, Accessible, Interoperable and Re-usable],
+                  L2[Mostly FAIR],
+                  L3[Somewhat FAIR],
+                  L4[UnFAIR data].
 
 Assertions: consists of InRepository, IsSensitive.
 
