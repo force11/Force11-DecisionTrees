b23aa2e3e44b657efcfd2ae26b8a38dd662cf403
Michael Bar-Sinai <mich.barsinai@gmail.com>
2017-10-24 15:26:03 +0300

    Cleanup and updates for FORCE2017

---
+++ open-access-publishing/oaJournalChooser.ts
@@ -0,0 +1,12 @@
+<* Tag suit for choosing an open access journal 
+*>
+
+DataTags: consists of complete, preprint, preprintFeedback, journalKnown, licenses, licenseKnown, tcs.
+
+complete [has the paper been written?]: one of yes, no.
+preprint [has a preprint been posted?]: one of yes, no.
+preprintFeedback [did the author get preprint feedback?]: one of yes, no.
+journalKnown [has an OA journal been identified?]: one of oa, hybrid, notOpen, no.
+licenses: one of single, many.
+licenseKnown: one of yes, no.
+tcs [think check submit]: one of yes, no.
